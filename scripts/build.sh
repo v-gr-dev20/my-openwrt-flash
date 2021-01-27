7756b379284cd1e3135558eab78497e6778d4c17
v-gr-dev20 <v.gr.dev20@gmail.com>
2021-01-27 16:37:32 +0300

    Выделена в отдельный файл  функция поиска Dockerfile

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,77 +1,28 @@
 # !/usr/bin/bash
 # Скрипт запускает сборку docker-образа из Dockerfile.
-# Скрипт предполагает наличие Dockerfile в папке, которая передается параметром в командной строке.
+# Скрипт находит Dockerfile по параметру в командной строке.
 # Если параметров нет, то скрипт находит Dockerfile в текущей или родительской папке уровнем выше.
 
 function main()
 {
-	# этот суффикс будет добавлен при поиске к вариантам пути Dockerfile
-	typeset suffix=-docker-img
-	# упорядоченный список путей поиска Dockerfile
-	typeset -a searchPlan
-	# путь к Dockerfile
-	typeset projectPath=
-	# имя docker-образа
-	typeset projectName=
-	# имя Dockerfile
-	typeset DockerfileName=
+	local projectName projectPath DockerfileName DockerfilePath
+	# Находим файлы docker
+	GetProject $1 |{ read projectName; read projectPath; read DockerfileName; read DockerfilePath; }
 
-	# Формируем план поиска Dockerfile
-	if [ -z "$1" ] ;then
-		searchPlan=( "${searchPlan[@]}" "$( pwd )" )
-		searchPlan=( "${searchPlan[@]}" "$( dirname "$( readlink -m "$0" )" )" )
-		searchPlan=( "${searchPlan[@]}" "$( dirname "${searchPlan[-1]}" )" )
-	elif [[ "$1" == "$( basename "$1" )" && ! $1 =~ ^(\.|\.\.|\/)$ ]] ;then
-		searchPlan=( "${searchPlan[@]}" "$( readlink -m "$1" )" )
-		searchPlan=( "${searchPlan[@]}" "${searchPlan[-1]}${suffix}" )
-		searchPlan=( "${searchPlan[@]}" "$( pwd )" )
-		searchPlan=( "${searchPlan[@]}" "$( dirname "$( readlink -m "$0" )" )/$1" )
-		searchPlan=( "${searchPlan[@]}" "${searchPlan[-1]}${suffix}" )
-		searchPlan=( "${searchPlan[@]}" "$( dirname "$( readlink -m "$0" )" )" )
-		searchPlan=( "${searchPlan[@]}" "$( dirname "${searchPlan[-1]}" )/$1" )
-		searchPlan=( "${searchPlan[@]}" "${searchPlan[-1]}${suffix}" )
-		searchPlan=( "${searchPlan[@]}" "$( dirname "$( dirname "$( readlink -m "$0" )" )" )" )
-		searchPlan=( "${searchPlan[@]}" "$( readlink -m "../$1" )" )
-		searchPlan=( "${searchPlan[@]}" "${searchPlan[-1]}${suffix}" )
-		projectName=$1
-	else
-		searchPlan=( "${searchPlan[@]}" "$( readlink -m "$1" )" )
-	fi
+	[ -n "$DockerfilePath" ] && { 
+		echo "${projectName}: ${DockerfilePath}"
 
-	# Находим Dockerfile проекта
-	for projectPath in "${searchPlan[@]}" ;do
-		[ -f "$projectPath" ] && break
-		[[ -d "$projectPath" && -f "$projectPath/Dockerfile" ]] && break
-	done
-
-	# Получаем имя docker-образа
-	projectName=${projectName:-$( echo "${projectPath}" |sed 's/^\///' )}
-	while [ "$( basename "$projectName" )" == "Dockerfile" ] ;do
-		projectName=$( dirname "$projectName" )
-	done
-	projectName=$( basename "$projectName" |sed 's/\('${suffix}'\)\{1,\}$//g' |sed 's/^[\.\/]$//g' |tr -s '[:upper:]' '[:lower:]' )
-
-	# Получаем путь и имя Dockerfile
-	if [ -f "$projectPath" ] ;then
-		DockerfileName=$( basename "$projectPath" )
-		projectPath=$( dirname "$projectPath" )
-	elif [[ -d "$projectPath" && -f "$projectPath/Dockerfile" ]] ;then
-		DockerfileName=Dockerfile
-	else
-	# либо выбрасываем ошибку
-		echo "Error: Dockerfile project not found!"
-		exit 1
-	fi
-
-	# Запускаем сборку docker-образа
-	echo "$projectPath/$DockerfileName"
-	docker build ${projectName:+-t "$projectName"} -f  "$projectPath/$DockerfileName" "$projectPath"
+		# Запускаем сборку docker-образа
+		docker build ${projectName:+-t "$projectName"} -f  "$projectPath/$DockerfileName" "$projectPath"
+	} || {
+		echo "Dockerfile not found." >&2
+	}
 }
 
 # Выводит подсказку
 function outputHelp()
 {
-	commandName=$( basename "$0" )
+	commandName=$( basename "$ThisScriptPath" )
 	if [ ".sh" == ".${commandName##*.}" ] ;then
 		commandName="${commandName%.*}"
 	fi
@@ -84,9 +35,13 @@ function outputHelp()
 }
 
 # Точка входа
+typeset ThisScriptPath="$( readlink -m "$0" )"
 if( [ -z "$1" ] || echo "$1" |grep -E '\-h|\-help' > /dev/null );then
 	outputHelp
 	exit
 fi
+# include
+. "$( dirname "$ThisScriptPath" )/common.sh" 2> /dev/null || \
+. "$( dirname "$( dirname "$ThisScriptPath" )" )/scripts/common.sh" 2> /dev/null
 
 main "$@"
---
+++ scripts/common.sh
@@ -0,0 +1,79 @@
+# !/usr/bin/bash
+
+# Находит Dockerfile в текущей или родительской папке уровнем выше.
+# Возвращает строки (в stdout)
+#	1 - имя проекта (docker-образа)
+#	2 - полный путь к папке с <Dockerfile>
+#	3 - имя файла <Dockerfile>
+#	4 - полный путь к <Dockerfile>
+# На входе:
+#	$1	- один из вариантов
+#		- пусто
+#		- путь к папке или файлу <Dockerfile>
+#		- ключевое имя для поиска файла или папки с <Dockerfile>
+function GetProject()
+{
+	# строки результата
+	local -a result
+	# этот суффикс будет добавлен при поиске к вариантам пути Dockerfile
+	local suffix=-docker-img
+	# упорядоченный список путей поиска Dockerfile
+	local -a searchPlan
+	# путь к Dockerfile
+	local projectPath=
+	# имя docker-образа
+	local projectName=
+	# имя Dockerfile
+	local DockerfileName=
+
+	# Формируем план поиска Dockerfile
+	if [ -z "$1" ] ;then
+		searchPlan=( "${searchPlan[@]}" "$( pwd )" )
+		searchPlan=( "${searchPlan[@]}" "$( dirname "$( readlink -m "$0" )" )" )
+		searchPlan=( "${searchPlan[@]}" "$( dirname "${searchPlan[-1]}" )" )
+	elif [[ "$1" == "$( basename "$1" )" && ! $1 =~ ^(\.|\.\.|\/)$ ]] ;then
+		searchPlan=( "${searchPlan[@]}" "$( readlink -m "$1" )" )
+		searchPlan=( "${searchPlan[@]}" "${searchPlan[-1]}${suffix}" )
+		searchPlan=( "${searchPlan[@]}" "$( pwd )" )
+		searchPlan=( "${searchPlan[@]}" "$( dirname "$( readlink -m "$0" )" )/$1" )
+		searchPlan=( "${searchPlan[@]}" "${searchPlan[-1]}${suffix}" )
+		searchPlan=( "${searchPlan[@]}" "$( dirname "$( readlink -m "$0" )" )" )
+		searchPlan=( "${searchPlan[@]}" "$( dirname "${searchPlan[-1]}" )/$1" )
+		searchPlan=( "${searchPlan[@]}" "${searchPlan[-1]}${suffix}" )
+		searchPlan=( "${searchPlan[@]}" "$( dirname "$( dirname "$( readlink -m "$0" )" )" )" )
+		searchPlan=( "${searchPlan[@]}" "$( readlink -m "../$1" )" )
+		searchPlan=( "${searchPlan[@]}" "${searchPlan[-1]}${suffix}" )
+		projectName=$1
+	elif [[ "$1" == "$( readlink -m "$1" )" ]] ;then
+		searchPlan=( "${searchPlan[@]}" "$1" )
+	else
+		searchPlan=( "${searchPlan[@]}" "$( readlink -m "$1" )" )
+		searchPlan=( "${searchPlan[@]}" "$( dirname "$( readlink -m "$0" )" )/$1" )
+	fi
+
+	# Находим Dockerfile проекта
+	result=( "${result[@]}" "" )
+	for projectPath in "${searchPlan[@]}" ;do
+		[ -f "$projectPath" ] && {
+			result[-1]="$projectPath"
+			break
+		}
+		[[ -d "$projectPath" && -f "$projectPath/Dockerfile" ]] && {
+			result[-1]="$projectPath/Dockerfile"
+			break
+		}
+	done
+
+	# Получаем имя docker-образа
+	projectName=${projectName:-$( echo "${result[-1]}" |sed 's/^\///' )}
+	if [ "$( basename "$projectName" )" == "Dockerfile" ] ;then
+		projectName=$( dirname "$projectName" )
+	fi
+	projectName=$( basename "$projectName" |sed 's/\('${suffix}'\)\{1,\}$//g' |sed 's/^[\.\/]$//g' |tr -s '[:upper:]' '[:lower:]' )
+
+	# выдаем строки результата
+	result=( "$projectName" "$( dirname "${result[-1]}" )" "$( basename "${result[-1]}" )" "${result[@]}" )
+	for item in "${result[@]}" ;do
+		echo "$item"
+	done
+}
