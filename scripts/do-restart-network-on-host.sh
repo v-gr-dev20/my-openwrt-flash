2287eeba1b667017fb4914c1326af23a2145e532
v-gr-dev20 <v.gr.dev20@gmail.com>
2021-02-05 14:38:54 +0300

    Усовершенствован скрипт рестарта сети

--- scripts/do-restart-network-on-host.sh
+++ scripts/do-restart-network-on-host.sh
@@ -5,27 +5,35 @@
 # доступность хоста покажет наличие подключения к сети
 pingHost=8.8.8.8
 # время в секундах до принудительной перезагрузки хоста в случае отсутствия подключения к сети
-patienceTimeout=60
-# время в секундах меджу контрольными тестами на подключение
-testTimeout=5
+patienceTimeout=120
 
 try() {
-	ping -c1 -w$( expr $patienceTimeout * 10 / 100 ) $pingHost
+	ping -c1 -w$( expr $patienceTimeout \* 10 / 100 ) $pingHost
 }
 
-# рестарт сети на удаленном хосте и перезагрузка после неудачи
-(((/etc/init.d/network restart &) ;sleep $patienceTimeout ;try > /dev/null 2>&1 || reboot )&)& 
+case "$1" in
 
-ps -w |grep -E "network|sleep" |grep -v grep
+	restart )
+		sh $0 check &
+		local checkPid=$!
+		# рестарт сети на удаленном хосте
+		/etc/init.d/network restart
+		/etc/init.d/firewall restart
+		/etc/init.d/dnsmasq restart
+		try || reboot
+		kill -9 $checkPid
+	;;
 
-# контроль подключения
-tryRest=$( expr $patienceTimeout / $testTimeout + 1 )
-while [[ 0 -lt $tryRest ]]
-do
-	sleep $testTimeout
-	if try ;then break ;fi
-	tryRest=$( expr $tryRest - 1 )
-done |while read line ;do
-	# контрольный вывод в случае сохранения подключения к терминалу
-	echo "$( date '+%F %H:%M:%S' )	$line"
-done
+	check )
+		# проверка и перезагрузка в случае неудачи
+		sleep $patienceTimeout
+		try || reboot
+	;;
+
+	* )
+		sh $0 restart > /tmp/$( basename $0 |sed 's/\.[^\.]*$//g' ).log & 
+		ps -w |grep -E "network|sleep" |grep -v grep
+	;;
+
+esac
+
