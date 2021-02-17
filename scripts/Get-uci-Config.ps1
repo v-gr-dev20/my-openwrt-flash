d6a2b6eb346f293f516611ccea8f52fe88906bfd
v-gr-dev20 <v.gr.dev20@gmail.com>
2021-02-17 13:52:44 +0300

    Явно задан контекст script для глобальной переменной

--- scripts/Get-uci-Config.ps1
+++ scripts/Get-uci-Config.ps1
@@ -41,5 +41,5 @@ if( 1 -lt $Args.Count -or 0 -eq $Args.Count -or $Args[0].ToLower() -in @( "-h", 
 	outputHelp
 	exit
 }
-New-Variable -Name config  -Value ( getConfig $Args[0] ) -Option ReadOnly
+New-Variable -Scope script -Name config  -Value ( getConfig $Args[0] ) -Option ReadOnly
 main( $Args | Select-Object -Skip 1 )
