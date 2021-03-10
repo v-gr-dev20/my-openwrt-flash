3994432cb6d0b82124222885116271d503817787
v-gr-dev20 <v.gr.dev20@gmail.com>
2021-03-10 15:34:02 +0300

    Присвоены hostname, timezone, passwd; получен публичный ключ

---
+++ DIR-320-820D/step01-uci-set-hostname+timezone.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+uci -q batch << EOF
+
+# system.@system[0]=system
+set system.@system[0].hostname='DIR320-820D'
+set system.@system[0].zonename='Europe/Kiev'
+set system.@system[0].timezone='EET-2EEST,M3.5.0/3,M10.5.0/4'
+
+EOF
+
+uci commit system || exit 1
+reboot
