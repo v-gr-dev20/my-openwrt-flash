7ae46ab4ba3b0df2651c32f21b0d03c48398a3f1
v-gr-dev20 <v.gr.dev20@gmail.com>
2021-03-11 14:52:54 +0300

    Добавлено соединение с административным хостом # Необходимо заранее добавить публичный ключ хоста-клиента в ~/.ssh/authorized_keys на административном хосте

---
+++ Mikrotik-hAPac2-FD92/add02-socks-proxy-luci-entrance-cmdl.ps1
@@ -0,0 +1,3 @@
+# !Powershell
+# Туннель через промежуточный хост RT-AC87 для входа через порт 8888 socks-proxy в web-GUI целевого хоста DIR-320-820D, также соединенного с промежуточным хостом RT-AC87U
+while( 1 ) { ssh -fTN -D8888 '-oServerAliveInterval=30' '-Jadmin@grigorovich-fam4.ddns.net' '-p8022' root@127.0.0.1; sleep 10; }
