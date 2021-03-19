c5514667813eb964e8313913e8b4a3dfd53d8f8e
v-gr-dev20 <v.gr.dev20@gmail.com>
2021-03-20 00:41:47 +0300

    Добавлен скрипт для извлечения opkg-пакетов из сборки openwrt

---
+++ openwrt-Broadcom-b43-dev-docker-img/get-packages.sh
@@ -0,0 +1,64 @@
+# !/usr/bin/bash
+# Скрипт извлекает из контейнера сборки openwrt opkg-пакеты и ключи, которыми подписаны пакеты
+
+openwrtRootInContainer=/root
+dir1InContainer=./openwrt/bin/targets/brcm47xx/legacy/packages
+dir2InContainer=./openwrt/bin/packages/mipsel_mips32/base
+dir3InContainer=./openwrt/build_dir/target-mipsel_mips32_musl/root.orig-brcm47xx/etc/opkg/keys
+tarFile=openwrt-mipsel_mips32-export-packages.tgz
+
+main() {
+	# определяем параметры проекта (находим Dockerfile, получаем имя Docker-образа)
+	local -a projectData
+	readarray -t projectData < <( GetProject )
+	local projectName=${projectData[0]}
+
+	# определяем источник и цель
+	local realTargetPath=${1:-.}
+	local realTargetDir=
+	local targetFile=
+	[ -d "$realTargetPath" -o "/" == "${realTargetPath: -1}" ] && {
+		realTargetDir=$( readlink -m "$realTargetPath" )
+		realTargetPath=${realTargetDir}
+	} || {
+		realTargetPath=$( readlink -m "$realTargetPath" )
+		realTargetDir=$( dirname "$realTargetPath" )
+		targetFile=$( basename "$realTargetPath" )
+	}
+	[ ! -d "$realTargetDir" ] && {
+		mkdir -p "${realTargetDir}"
+	}
+	local mappedTargetDir=/mnt/volatile-target-dir
+
+	# копирование
+	echo mount $realTargetDir '->' $mappedTargetDir
+	docker run --rm -it -v ${realTargetDir}:${mappedTargetDir} "$projectName" bash -c \
+		"tar -czf ${tarFile} -C ${openwrtRootInContainer} ${dir1InContainer} ${dir2InContainer} ${dir3InContainer} && cp -v ${tarFile} ${mappedTargetDir}/${tarFile}"
+}
+
+# Выводит подсказку
+function outputHelp()
+{
+	commandName=$( basename "$ThisScriptPath" )
+	if [ ".sh" == ".${commandName##*.}" ] ;then
+		commandName="${commandName%.*}"
+	fi
+	echo \
+"	Usage:
+		$commandName [ <destination> ]
+		$commandName -h | --help
+"
+}
+
+# Точка входа
+typeset ThisScriptPath="$( readlink -m "$0" )"
+if( echo "$1" |grep -E '\-h|\-\-help' > /dev/null );then
+	outputHelp
+	exit
+fi
+# include
+i="$( dirname "$ThisScriptPath" )/common.sh" ;[ -f "$i" ] && . "$i"
+i="$( dirname "$( dirname "$ThisScriptPath" )" )/scripts/common.sh" ;[ -f "$i" ] && . "$i"
+echo "$( dirname "$( dirname "$ThisScriptPath" )" )/scripts/common.sh" >&2
+
+main "$@"
