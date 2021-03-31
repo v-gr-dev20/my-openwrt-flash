77782f5395cbbc0acaf199505a45305c687bdcb8
v-gr-dev20 <v.gr.dev20@gmail.com>
2021-03-31 18:06:32 +0300

    Настроен DDNS

---
+++ TL-MR3420-FCB6/step03-uci-setup-ddns.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+uci -q batch << EOF
+
+set ddns.myddns_ipv4.lookup_host='grigorovich-fam5.ddns.net'
+set ddns.myddns_ipv4.domain='grigorovich-fam5.ddns.net'
+set ddns.myddns_ipv4.username='grigorovich-fam'
+set ddns.myddns_ipv4.password='password'\''s changed'
+set ddns.myddns_ipv4.interface='wan'
+set ddns.myddns_ipv4.ip_source='web'
+set ddns.myddns_ipv4.service_name='no-ip.com'
+set ddns.myddns_ipv4.enabled='1'
+set ddns.myddns_ipv4.use_https='0'
+
+set ddns.myddns_ipv6.enabled='0'
+
+EOF
+
+uci commit ddns || exit 1
+/etc/init.d/ddns enable
+/etc/init.d/ddns restart
