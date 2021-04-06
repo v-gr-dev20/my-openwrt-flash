08b19a0b0e87d158aeda7fb540005c5b43b34ef6
v-gr-dev20 <v.gr.dev20@gmail.com>
2021-04-06 16:51:11 +0300

    Загружены в /home opkg-пакеты из локальной сборки openwrt

---
+++ DIR-320-E768/step13-put-packages.ps1
@@ -0,0 +1,62 @@
+# !Powershell
+# Скрипт загружает на удаленный хост в папку /home расширенный набор opkg-пакетов и ключ, которым подписаны пакеты
+
+# Будут распакованы файлы:
+#	./openwrt/build_dir/target-mipsel_mips32_musl/openwrt-imagebuilder-brcm47xx-legacy.Linux-x86_64/packages
+#	./openwrt/build_dir/target-mipsel_mips32_musl/root.orig-brcm47xx/etc/opkg/keys/7d39be47dd13cf04
+$tarFile = 'openwrt-mipsel_mips32-export-packages.tgz'
+$targetDirOnHost = '/home'
+
+function main( [string] $tarFilePath = $tarFile )
+{
+	$anURNpartOfConfig = getURNpartFromConfig $config
+	putPackages $anURNpartOfConfig -tarFilePath:$tarFilePath
+}
+
+# include
+. $( Join-Path -Path "$( $MyInvocation.MyCommand.Path |Split-Path -parent |Split-Path -parent )" -ChildPath "scripts/common.ps1" )
+. $( Join-Path -Path "$( $MyInvocation.MyCommand.Path |Split-Path -parent |Split-Path -parent )" -ChildPath "scripts/ssh-functions.ps1" )
+
+# Выполняет копирование и распаковку opkg-пакетов
+function putPackages( [Parameter( Mandatory, Position = 0 )] $config,
+	[string] $tarFilePath )
+{
+	Invoke-Command-by-SSH $config -Description:"Packages transfer to remote host" `
+			-RedirectStandardInput:$tarFilePath `
+			'tar' 'xzf','-',$( '-C"' + $targetDirOnHost + '"' )
+}
+
+# Выводит подсказку
+function outputHelp()
+{
+	$commandName = $ThisScriptPath |Split-Path -Leaf
+	if( ".ps1" -eq [System.IO.Path]::GetExtension( $commandName ).ToLower() ) {
+		$commandName = [System.IO.Path]::GetFileNameWithoutExtension( $commandName )
+	}
+"	Usage:
+		$commandName [ <device> ] [ /path/to/packages.tgz ]
+		$commandName	-h | --help
+"
+}
+
+# Точка входа
+[string] $ThisScriptPath = $MyInvocation.MyCommand.Path
+if( ( 1 -le $Args.Count -and $Args[0].ToLower() -in @( "-h", "--help" ) ) `
+	-or ( 3 -lt $Args.Count ) `
+	-or ( 2 -eq $Args.Count -and -not ( $Args[0].ToLower() -in @( "-k", "--keypath" ) ) ) `
+	-or ( 3 -eq $Args.Count -and -not ( $Args[1].ToLower() -in @( "-k", "--keypath" ) ) ) )
+{
+	outputHelp
+	exit
+}
+$toSkipArgsCount = 0
+New-Variable -Scope script -Name config  -Value $(
+	# интерпретируем контекст аргументов скрипта, см. Usage:
+	if( 0 -eq $Args.Count -or ( $Args[0].ToLower() -in @( "-k", "--keypath" ) ) ) {
+		getConfig
+	} else {
+		$toSkipArgsCount += 1
+		getConfig $Args[0]
+	}
+)
+Invoke-Command { main @Args } -ArgumentList ( $Args |Select-Object -Skip $toSkipArgsCount )
