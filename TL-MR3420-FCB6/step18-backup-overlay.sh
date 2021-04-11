c37da782ace7322b6087dbed7040af195ad64f9d
v-gr-dev20 <v.gr.dev20@gmail.com>
2021-04-11 21:51:45 +0300

    Добавлен скрипт для архивирования extroot:/overlay

---
+++ TL-MR3420-FCB6/step18-backup-overlay.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+tarName="$( date '+%Y-%m-%d-%H%M%S' )-$( cat /proc/sys/kernel/hostname )-overlay.tgz"
+mkdir -p /home/backup &> /dev/null || exit 1
+( cd /overlay || exit 1 )
+( cd /rwm || exit 1 )
+tar -czf "/home/backup/${tarName}" /overlay /rwm
+cat "/home/backup/${tarName}" |ssh -y -i /etc/dropbear/dropbear_rsa_host_key admin@grigorovich-fam4.ddns.net "( mkdir -p /tmp/mnt/data/backup ;cat - > ""/tmp/mnt/data/backup/${tarName}"" )"
