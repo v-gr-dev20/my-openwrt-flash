ce88746238beeb41052249fe6105dedc7601cc41
v-gr-dev20 <v.gr.dev20@gmail.com>
2021-06-02 16:07:55 +0300

    Убрано форсирование запуска апгрейда (без проверки)

--- DIR-320-ALL/Sysupgrade.ps1
+++ DIR-320-ALL/Sysupgrade.ps1
@@ -21,7 +21,7 @@ function sysupgrade( [Parameter( Position = 0 )] $config, [Parameter( Position =
 	<#assert#> if( 0 -eq $anURNsChain.Count ) { throw }
 	$targetURN = $anURNsChain[-1]
 	Invoke-SCP $config $firmwareBin "${targetURN}:${firmwarePathOnRemoteHost}" `
-	&& Invoke-Command-by-SSH $config "sysupgrade" `-n `-v `-F $firmwarePathOnRemoteHost |%{ "$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')`t$_" }
+	&& Invoke-Command-by-SSH $config "sysupgrade" `-n `-v $firmwarePathOnRemoteHost |%{ "$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')`t$_" }
 }
 
 # Выводит подсказку
