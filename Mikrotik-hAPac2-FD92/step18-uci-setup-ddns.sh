628f112ea8b036744443ddf0956a2ea32adc8151
v-gr-dev20 <v.gr.dev20@gmail.com>
2021-06-14 21:33:18 +0300

    Добавлен и настроен DDNS

---
+++ Mikrotik-hAPac2-FD92/step18-uci-setup-ddns.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+uci -q batch << EOF
+
+set ddns.myddns_ipv4.lookup_host='grigorovich-fam.ddns.net'
+set ddns.myddns_ipv4.domain='grigorovich-fam.ddns.net'
+set ddns.myddns_ipv4.username='grigorovich-fam'
+set ddns.myddns_ipv4.password='password'\''s changed'
+set ddns.myddns_ipv4.interface='lte'
+set ddns.myddns_ipv4.ip_source='web'
+set ddns.myddns_ipv4.service_name='no-ip.com'
+set ddns.myddns_ipv4.enabled='1'
+set ddns.myddns_ipv4.use_https='1'
+delete ddns.myddns_ipv4.ip_network
+
+set ddns.myddns_ipv6.enabled='0'
+
+EOF
+
+uci commit ddns || exit 1
+/etc/init.d/ddns enable
+/etc/init.d/ddns restart
