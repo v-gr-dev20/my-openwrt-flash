e02de76707189477efbc37bc9e86dd9ab443ed7a
v-gr-dev20 <v.gr.dev20@gmail.com>
2022-03-27 15:01:33 +0300

    Учтены изменения схемы подключения

--- Mikrotik-hAPac2-FD92/add01-ssh-entrance-cmdl.ps1
+++ Mikrotik-hAPac2-FD92/add01-ssh-entrance-cmdl.ps1
@@ -1,3 +1,4 @@
 # !Powershell
-# Вход в консоль удаленного хоста DIR-320-820D, подключенного к промежуточному хосту RT-AC87U
-while( 1 ) { ssh '-oServerAliveInterval=30' '-Jadmin@grigorovich4.ddns.net' '-p8022' root@127.0.0.1; sleep 10; }
+# Вход в консоль удаленного хоста
+# while( 1 ) { ssh '-oServerAliveInterval=30' '-Jadmin@grigorovich4.ddns.net' '-p8022' root@127.0.0.1; sleep 10; }
+while( 1 ) { ssh '-Jroot@127.0.0.1:2222' '-p8022' root@127.0.0.1; sleep 10; }
--- Mikrotik-hAPac2-FD92/add02-socks-proxy-luci-entrance-cmdl.ps1
+++ Mikrotik-hAPac2-FD92/add02-socks-proxy-luci-entrance-cmdl.ps1
@@ -1,3 +1,4 @@
 # !Powershell
-# Туннель через промежуточный хост RT-AC87 для входа через порт 8888 socks-proxy в web-GUI целевого хоста DIR-320-820D, также соединенного с промежуточным хостом RT-AC87U
-while( 1 ) { ssh -fTN -D8888 '-oServerAliveInterval=30' '-Jadmin@grigorovich4.ddns.net' '-p8022' root@127.0.0.1; sleep 10; }
+# Туннель через промежуточный хост для входа через порт 8888 socks-proxy в web-GUI целевого хоста, также соединенного с промежуточным хостом
+# while( 1 ) { ssh -fTN -D8888 '-oServerAliveInterval=30' '-Jadmin@grigorovich4.ddns.net' '-p8022' root@127.0.0.1; sleep 10; }
+while( 1 ) { ssh -fTN -D8888 '-oServerAliveInterval=30' '-Jroot@127.0.0.1:2222' '-p8022' root@127.0.0.1; sleep 10; }
