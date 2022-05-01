198506653e419182211971ffbe0efd3e1599e545
v-gr-dev20 <v.gr.dev20@gmail.com>
2022-05-01 20:55:31 +0300

    Присвоены hostname, timezone, passwd; получен публичный ключ

---
+++ Mikrotik-hAPac2-D663/step01-uci-set-hostname+timezone.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+uci -q batch << EOF
+
+# system.@system[0]=system
+set system.@system[0].hostname='Mikrotik-hAPac2-D663'
+set system.@system[0].zonename='Europe/Kiev'
+set system.@system[0].timezone='EET-2EEST,M3.5.0/3,M10.5.0/4'
+
+EOF
+
+uci commit system || exit 1
+reboot
