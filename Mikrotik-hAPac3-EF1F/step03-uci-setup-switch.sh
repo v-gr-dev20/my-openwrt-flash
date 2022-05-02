ffb4b853dbcde0f17b7ff8d7e5b7609f1000d74b
v-gr-dev20 <v.gr.dev20@gmail.com>
2022-05-02 14:59:50 +0300

    Включен switch, определены vlan, определены уникальные mac-адреса

---
+++ Mikrotik-hAPac3-EF1F/step03-uci-setup-switch.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+
+getSwitch()
+{
+	swconfig list |sed 's/^\S\+\s\+\(\S\+\)\s.*$/\1/g'
+}
+
+while uci delete network.@switch[-1] 2> /dev/null ;do continue ;done
+switch=$( getSwitch )
+uci get network.@switch[0] > /dev/null && exit 0
+while uci delete network.@switch_vlan[-1] 2> /dev/null ;do continue ;done
+
+uci -q batch << EOF
+
+add network switch
+set network.@switch[-1]=switch
+set network.@switch[-1].name='$switch'
+set network.@switch[-1].reset='1'
+set network.@switch[-1].enable_vlan='0'
+
+EOF
+
+uci commit network || exit 1
