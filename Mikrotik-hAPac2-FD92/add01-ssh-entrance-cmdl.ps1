9e5d1dbb0a3bc5bfe200247fa80c3ac65fb86121
v-gr-dev20 <v.gr.dev20@gmail.com>
2023-05-07 00:21:28 +0300

    Добавлены способы доступа через ssh

--- Mikrotik-hAPac2-FD92/add01-ssh-entrance-cmdl.ps1
+++ Mikrotik-hAPac2-FD92/add01-ssh-entrance-cmdl.ps1
@@ -1,4 +1,8 @@
 # !Powershell
 # Вход в консоль удаленного хоста
-# while( 1 ) { ssh '-oServerAliveInterval=30' '-Jadmin@grigorovich4.freeddns.org' '-p8022' root@127.0.0.1; sleep 10; }
-while( 1 ) { ssh '-Jroot@127.0.0.1:2222' '-p8022' root@127.0.0.1; sleep 10; }
+while( 1 ) {
+	ssh '-oServerAliveInterval=30' '-Jroot@grigorovich5.freeddns.org' -p8022 root@127.0.0.1
+	ssh '-oServerAliveInterval=30' '-Jroot@127.0.0.1:2250' -p8022 root@127.0.0.1
+	ssh '-oServerAliveInterval=30' -p2220 root@127.0.0.1
+	sleep 10
+}
