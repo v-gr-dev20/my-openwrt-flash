88c34324a10e2714c8ac5a2d24b508815344d96d
v-gr-dev20 <v.gr.dev20@gmail.com>
2023-05-06 23:29:37 +0300

    Изменен DDNS

--- Mikrotik-hAPac2-D663/add01-ssh-entrance-cmdl.ps1
+++ Mikrotik-hAPac2-D663/add01-ssh-entrance-cmdl.ps1
@@ -1,3 +1,7 @@
 # !Powershell
 # Вход в консоль удаленного хоста
-while( 1 ) { ssh '-Jroot@127.0.0.1:2222' root@192.168.50.1; sleep 10; }
+while( 1 ) {
+#	ssh '-Jroot@127.0.0.1:2250' root@192.168.50.1
+	ssh '-Jroot@grigorovich5.freeddns.org' root@192.168.50.1
+	sleep 10
+}
--- Mikrotik-hAPac2-D663/add02-socks-proxy-luci-entrance-cmdl.ps1
+++ Mikrotik-hAPac2-D663/add02-socks-proxy-luci-entrance-cmdl.ps1
@@ -1,3 +1,7 @@
 # !Powershell
 # Туннель через промежуточный хост RT-AC87 для входа через порт 8888 socks-proxy в web-GUI целевого хоста, также соединенного с промежуточным хостом RT-AC87U
-while( 1 ) { ssh -fTN -D8888 '-oServerAliveInterval=30' '-Jroot@127.0.0.1:2222' root@192.168.50.1; sleep 10; }
+while( 1 ) {
+#	ssh -fTN -D8850 '-oServerAliveInterval=30' '-Jroot@127.0.0.1:2250' root@192.168.50.1
+	ssh -fTN -D8850 '-oServerAliveInterval=30' '-Jroot@grigorovich5.freeddns.org' root@192.168.50.1
+	sleep 10
+}
