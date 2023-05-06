a0a4083e3fed876fc64e5220eef22661bff80242
v-gr-dev20 <v.gr.dev20@gmail.com>
2023-05-07 00:11:04 +0300

    Изменен DDNS

--- Mikrotik-hAPac2-FD92/add01-ssh-entrance-cmdl.ps1
+++ Mikrotik-hAPac2-FD92/add01-ssh-entrance-cmdl.ps1
@@ -1,4 +1,4 @@
 # !Powershell
 # Вход в консоль удаленного хоста
-# while( 1 ) { ssh '-oServerAliveInterval=30' '-Jadmin@grigorovich4.ddns.net' '-p8022' root@127.0.0.1; sleep 10; }
+# while( 1 ) { ssh '-oServerAliveInterval=30' '-Jadmin@grigorovich4.freeddns.org' '-p8022' root@127.0.0.1; sleep 10; }
 while( 1 ) { ssh '-Jroot@127.0.0.1:2222' '-p8022' root@127.0.0.1; sleep 10; }
