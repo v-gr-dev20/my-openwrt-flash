40d246c1b26ae7188e9f7f81180086f8f48576ea
v-gr-dev20 <v.gr.dev20@gmail.com>
2023-06-18 08:50:14 +0300

    save

--- OpenWrt-amd64-docker/step03-start-openwrt.ps1
+++ OpenWrt-amd64-docker/step03-start-openwrt.ps1
@@ -5,8 +5,16 @@ if( ! $? ) {
 	docker network create mynetwork
 }
 
+#docker run --rm --detach --name openwrt `
+#	--privileged --device /dev/net/tun `
+#	--cap-add=NET_ADMIN `
+#	-p 8022:22 -p 8080:80 `
+#	--network="mynetwork" `
+#	-it openwrt
+
+
 docker run --rm --detach --name openwrt `
-	--privileged --device /dev/net/tun `
+	--privileged `
 	--cap-add=NET_ADMIN `
 	-p 8022:22 -p 8080:80 `
 	--network="mynetwork" `
---
+++ OpenWrt-amd64-docker/step05-uci-setup-wan.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+uci show network.wan &>/dev/null && exit 0
+
+uci -q batch << EOF
+
+add network device
+set network.@device[-1]=device
+set network.@device[-1].name='eth1'
+
+set network.wan=interface
+set network.wan.ifname='eth1'
+set network.wan.proto='static'
+set network.wan.ipaddr='172.20.0.2'
+set network.wan.netmask='255.255.0.0'
+set network.wan.gateway='172.20.0.1'
+set network.wan.ip6assign='60'
+
+EOF
+
+echo 001
+uci commit || exit 1
+#uci commit network || exit 1
+echo 002
+#uci commit firewall || exit 1
+/etc/init.d/network reload
+#/etc/init.d/network restart
+#/etc/init.d/firewall restart
